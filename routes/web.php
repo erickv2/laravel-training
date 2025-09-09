ef9ccc0d894ee2eda153e992ab7e7d31fbeec57b
gitcore33 <erickestanislau25@gmail.com>
2025-09-09 13:06:26 -0300

    any and match routes

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 Route::get('/empresa', function(){
     return view('site/empresa');
 });
+
+Route::any('/any', function() {
+    return "Permite todo tipo de acesso http (put, delete, get, post)";
+});
+
+Route::match('/match', function(){
+    return "Permite apenas acessos definidos";
+});
