c4d374df97445340b162a5e81b32b045b3a9edad
gitcore33 <erickestanislau25@gmail.com>
2025-09-10 17:09:38 -0300

    grouping routes

--- routes/web.php
+++ routes/web.php
@@ -8,23 +8,27 @@ Route::get('/', function () {
 
 // ----------------------------------------------------
 
-// renderizando uma view
+// RENDERIZANDO VIEW
 
 // Route::get('/empresa', function(){
 //     return view('site/empresa');
 // });
 
-// forma mais simples de renderizar uma view
+// FORMA MAIS SIMPLES DE RENDERIZAR UMA VIEW
+
 Route::view('/empresa', 'site/empresa');
 
 // ----------------------------------------------------
 
-// aceita qualquer tipo de requisiçáo
-Route::any('/any', function() {
+// MATCH E ANY
+
+// ACEITA QUALQUER TIPO DE REQUISICAO
+
+Route::any('/any', function () {
     return "Permite todo tipo de acesso http (put, delete, get, post)";
 });
 
-//aceita apenas requisicoes menscionadas
+// ACEITA APENAS REQUISICOES MENCIONADAS
 
 // Route::match('/match', function(){
 //     return "Permite apenas acessos definidos";
@@ -32,32 +36,88 @@ Route::any('/any', function() {
 
 // ----------------------------------------------------
 
-// inserir parametros
+// PARAMETROS
 
-Route::get('/produto/{id}/{cat?}', function($id, $cat = ''){
+Route::get('/produto/{id}/{cat?}', function ($id, $cat = '') {
     return "O id do produto é: " . $id . "<br>" . "A categoria é: " . $cat;
 });
 
 // ----------------------------------------------------
 
-// redirecionamentos
+// REDIRECIONAMENTOS
 
 // Route::get('/sobre', function(){
 //     return redirect('/empresa');
 // });
 
-// forma mais simples de redirecionar
+// FORMA MAIS SIMPLES DE REDIRECIONAR
 
 Route::redirect('/sobre', '/empresa');
 
 // ----------------------------------------------------
 
-// nomeando rotas
+// NO9MEANDO ROTAS
 
-Route::get('/news', function(){
+Route::get('/news', function () {
     return view('news');
 })->name('noticias');
 
-Route::get('/novidades', function(){
+Route::get('/novidades', function () {
     return redirect()->route('noticias');
 });
+
+
+// ----------------------------------------------------
+
+Route::get('/', function () {
+    return redirect()->route('admin.clients');
+});
+
+// AGRUPANDO ROTAS
+
+// Route::prefix('admin')->group(function () {
+//     Route::get('dashboard', function () {
+//         return "dashboard";
+//     });
+//     Route::get('users', function () {
+//         return "users";
+//     });
+//     Route::get('clients', function () {
+//         return "clients";
+//     });
+// });
+
+// AGRUPANDO ROTAS NOMEADAS
+
+// Route::name('admin.')->group(function () {
+//     Route::get('admin/dashboard', function () {
+//         return "dashboard";
+//     })->name('dashboard');
+//     Route::get('admin/users', function () {
+//         return "users";
+//     })->name('users');
+//     Route::get('admin/clients', function () {
+//         return "clients";
+//     })->name('clients');
+// });
+
+
+// AGRUPANDO ROTAS POR PREFIXO E NOME AO MESMO TEMPO
+
+Route::group([
+    'prefix' => 'admin',
+    'as' => 'admin.'
+], function(){
+    Route::get('dashboard', function () {
+        return "dashboard";
+    })->name('dashboard');
+    Route::get('users', function () {
+        return "users";
+    })->name('users');
+    Route::get('clients', function () {
+        return "clients";
+    })->name('clients');
+});
+
+
+
