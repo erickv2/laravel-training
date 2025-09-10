9f09d189428734563040a35640a791b28b3c7230
gitcore33 <erickestanislau25@gmail.com>
2025-09-10 16:37:20 -0300

    redirect function and adds comments

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/empresa', function(){
-    return view('site/empresa');
-});
+// renderizando uma view
 
+// Route::get('/empresa', function(){
+//     return view('site/empresa');
+// });
+
+// forma mais simples de renderizar uma view
+Route::view('/empresa', 'site/empresa');
+
+// aceita qualquer tipo de requisiçáo
 Route::any('/any', function() {
     return "Permite todo tipo de acesso http (put, delete, get, post)";
 });
@@ -21,3 +27,14 @@ Route::any('/any', function() {
 Route::get('/produto/{id}/{cat?}', function($id, $cat = ''){
     return "O id do produto é: " . $id . "<br>" . "A categoria é: " . $cat;
 });
+
+// formas de redirecionar
+
+// Route::get('/sobre', function(){
+//     return redirect('/empresa');
+// });
+
+Route::redirect('/sobre', '/empresa');
+
+
+
