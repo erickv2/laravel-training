f75788628bb096df71bd9407d42aa8c526889dc7
gitcore33 <erickestanislau25@gmail.com>
2025-09-10 16:44:31 -0300

    naming routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// ----------------------------------------------------
+
 // renderizando uma view
 
 // Route::get('/empresa', function(){
@@ -15,26 +17,47 @@ Route::get('/', function () {
 // forma mais simples de renderizar uma view
 Route::view('/empresa', 'site/empresa');
 
+// ----------------------------------------------------
+
 // aceita qualquer tipo de requisiçáo
 Route::any('/any', function() {
     return "Permite todo tipo de acesso http (put, delete, get, post)";
 });
 
+//aceita apenas requisicoes menscionadas
+
 // Route::match('/match', function(){
 //     return "Permite apenas acessos definidos";
 // });
+
+// ----------------------------------------------------
+
+// inserir parametros
 
 Route::get('/produto/{id}/{cat?}', function($id, $cat = ''){
     return "O id do produto é: " . $id . "<br>" . "A categoria é: " . $cat;
 });
 
-// formas de redirecionar
+// ----------------------------------------------------
+
+// redirecionamentos
 
 // Route::get('/sobre', function(){
 //     return redirect('/empresa');
 // });
 
+// forma mais simples de redirecionar
+
 Route::redirect('/sobre', '/empresa');
 
+// ----------------------------------------------------
 
+// nomeando rotas
 
+Route::get('/news', function(){
+    return view('news');
+})->name('noticias');
+
+Route::get('/novidades', function(){
+    return redirect()->route('noticias');
+});
