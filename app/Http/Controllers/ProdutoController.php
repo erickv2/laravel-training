2893f3a1a6d63d6159b6393395932604a3abf61d
gitcore33 <erickestanislau25@gmail.com>
2025-09-11 13:44:32 -0300

    models

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \app\Models\Product;
 
 class ProdutoController extends Controller
 {
@@ -11,7 +12,8 @@ class ProdutoController extends Controller
      */
     public function index()
     {
-        return "index";
+        $produtos = Product::all();
+        return dd($produtos);
     }
 
     /**
