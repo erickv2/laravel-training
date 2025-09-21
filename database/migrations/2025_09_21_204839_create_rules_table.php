5c21883cb0e30b5339b6863eb94f6bffb1347cf0
gitcore33 <erickestanislau25@gmail.com>
2025-09-21 17:58:08 -0300

    creates contacts and rules migrations

---
+++ database/migrations/2025_09_21_204839_create_rules_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rules', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('rule_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->unsignedBigInteger('id_regra');
+            $table->foreign('id_regra')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rules');
+        Schema::dropIfExists('rule_user');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // UsersSeeder::class,
-            // CategoriesSeeder::class,
+            UsersSeeder::class,
+            CategoriesSeeder::class,
             ProductsSeeder::class,
         ]);
     }
